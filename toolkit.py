c9d374c00610ed50c174d7a9957350c89951587a
User117 <gunnar.andrews97@gmail.com>
2020-01-10 16:26:46 -0600

    split into domain and ip tools

--- toolkit.py
+++ toolkit.py
@@ -1,6 +1,8 @@
 import threading
 import os
 from pathlib import Path
+import argparse
+
 
 base_dir = Path(__file__).parent
 lepus_dir = Path(base_dir / 'Lepus')
@@ -11,35 +13,42 @@ else:
     py_command = "python3"
 
 
-def task1():
-    print("Task 1 assigned thread: {}".format(threading.current_thread().name))
-    print("ID of process running task 1: {}".format(os.getpid()))
-
-
-def task2(domain_):
+def nmap_task(domain_):
     os.system("nmap -sT -sV --version-intensity 0 -O -v --reason {} -oA {}.nmap -p 21,22,23,80,443,81,8443,8080,8081,8888,25,1403,3306,3389,135,139,445".format(domain_, domain_))
 
 
-def task3(domain_):
+def lepus_task(domain_):
     os.chdir(lepus_dir)
     subdomains_txt_path = os.path.join("lists", "subdomains.txt")
     os.system("{} lepus.py {} -w {} --permutate --reverse".format(py_command, domain_, subdomains_txt_path))
 
 
 if __name__ == "__main__":
-    print("ID of process running the main program: {}".format(os.getpid()))
-    print("Main Thread Name: {}".format(threading.main_thread().name))
+    # print("ID of process running the main program: {}".format(os.getpid()))
+    # print("Main Thread Name: {}".format(threading.main_thread().name))
 
-    domain = "4cmedicalgroup.com"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--domains", action="store", dest="domains", help="set a domain or list of domains",
+                        type=str, default=None)
+    parser.add_argument("-i", "--ips", action="store", dest="ips", help="set an ip or ip range",
+                        type=str, default=None)
+    args = parser.parse_args()
 
-    t1 = threading.Thread(target=task1, name='t1')
-    t2 = threading.Thread(target=task2, name='t2', args=[domain])
-    t3 = threading.Thread(target=task3, name='Lepus', args=[domain])
+    if args.domains:
+        print("running domain tools")
+        print(args.domains)
+        lepus = threading.Thread(target=lepus_task, name='Lepus', args=[args.domains])
+        lepus.start()
+    if args.ips:
+        print("running ip tools")
+        nmap = threading.Thread(target=nmap_task, name='Lepus', args=[args.ips])
+        nmap.start()
 
-    t1.start()
-    t2.start()
-    t3.start()
-
-    t1.join()
-    t2.join()
-    t3.join()
+    try:
+        lepus.join()
+    except NameError:
+        pass
+    try:
+        nmap.join()
+    except NameError:
+        pass
